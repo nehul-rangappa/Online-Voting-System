c0e9e4e7b5cda03615f47a0b817f566c47858e56
Nehul1 <44396665+Nehul1@users.noreply.github.com>
2019-04-04 22:18:49 +0530

    Update settings.py

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -14,8 +14,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'johns69mith69@gmail.com'
-EMAIL_HOST_PASSWORD = 'nehulmama'
 
 #DEFAULT_FROM_EMAIL = 'johns69mith69@gmail.com'
 
@@ -29,8 +27,7 @@ MEDIA_DIR=os.path.join(BASE_DIR,'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c&j5%&ytyn9hmyo(c7p2mona#r_xfzw$cw%fn2k!x-wtp&dif%'
-
+SECRET_KEY = #some security key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
